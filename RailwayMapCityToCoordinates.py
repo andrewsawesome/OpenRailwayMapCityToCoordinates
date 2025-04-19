d6b1137590a27369d93c2bbcfde03d0755416f28
andrewsawesome <36270171+andrewsawesome@users.noreply.github.com>
2025-04-19 10:55:01 -0400

    Update RailwayMapCityToCoordinates.py
    Added better error handling and more general language

--- RailwayMapCityToCoordinates.py
+++ RailwayMapCityToCoordinates.py
@@ -2,13 +2,19 @@ import webbrowser
 from geopy.geocoders import Nominatim
 geolocator = Nominatim(user_agent='myapplication')
 city = input("What city would you like to go to? ")
-state = input("In what state (Full name or two-letter abbreviation)? ")
-location = geolocator.geocode(f"{city} {state}")
+state = input("In what state/country (Full name or two-letter abbreviation)? ")
+try:
+    location = geolocator.geocode(f"{city} {state}")
+except Exception:
+    print("We could not load correctly. Please check your internet connection.")
 try:
     latitude = location.latitude
     longitude = location.longitude
 except AttributeError:
     print("We could not find your city. Please make sure your spelling is correct.")
+    latitude = 39.9527237
+    longitude = -75.1635262
+except Exception:
     latitude = 39.9527237
     longitude = -75.1635262
 print(latitude)
