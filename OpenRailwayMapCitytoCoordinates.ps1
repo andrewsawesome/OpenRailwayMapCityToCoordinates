6176ae896b629da5a2dbf293d7804eb086d00a53
andrewsawesome <36270171+andrewsawesome@users.noreply.github.com>
2026-02-26 16:24:08 -0500

    Translated the program to PowerShell

---
+++ OpenRailwayMapCitytoCoordinates.ps1
@@ -0,0 +1,13 @@
+﻿Install-Module -Name OSMGeocode -ErrorAction SilentlyContinue
+Import-Module OSMGeocode
+$city= [PSCustomObject]@{
+"postalcode" = 0
+}
+
+$city.postalcode = Read-Host "What is the postal code of the city?"
+
+$results=($($city | Invoke-OSM -Email "a@c.com" -ResultsLanguage "en"))
+$lat=$results.lat
+$lon=$results.lon
+
+Start-Process "https://openrailwaymap.app/#view=12.17/$lat/$lon"
