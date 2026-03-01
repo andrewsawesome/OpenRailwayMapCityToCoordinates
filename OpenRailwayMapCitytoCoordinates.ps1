d5f0b2c693c6251000cc01bc64a5a9430e6a8e95
andrewsawesome <36270171+andrewsawesome@users.noreply.github.com>
2026-03-01 15:05:47 -0500

    Update OpenRailwayMapCitytoCoordinates.ps1
    Updated to suppress errors better

--- OpenRailwayMapCitytoCoordinates.ps1
+++ OpenRailwayMapCitytoCoordinates.ps1
@@ -1,13 +1,14 @@
 ﻿Install-Module -Name OSMGeocode -ErrorAction SilentlyContinue
-Import-Module OSMGeocode
+Import-Module OSMGeocode -ErrorAction SilentlyContinue
 $city= [PSCustomObject]@{
 "postalcode" = 0
 }
 
 $city.postalcode = Read-Host "What is the postal code of the city?"
 
-$results=($($city | Invoke-OSM -Email "a@c.com" -ResultsLanguage "en"))
+$results=($($city | Invoke-OSM -Email "36270171+andrewsawesome@users.noreply.github.com" -ResultsLanguage "en"))
 $lat=$results.lat
 $lon=$results.lon
+
 
 Start-Process "https://openrailwaymap.app/#view=12.17/$lat/$lon"
